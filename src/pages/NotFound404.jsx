0d7fcc3419e42a255b1f246858cee5278afe0e18
ImeneB7 <imene.bel74@gmail.com>
2024-06-12 11:19:15 +0200

    ajout des composants

---
+++ src/pages/NotFound404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const NotFound404 = () => {
+    return (
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default NotFound404;

993d86aef7b73f7ef1b9f61c8cb8ced6232a27f2
ImeneB7 <imene.bel74@gmail.com>
2024-06-17 11:07:07 +0200

    modifications du css pour collapse, about et notfound404

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,1 +1,28 @@
 import '../sass/_collapse.scss';
+import arrowDown from '../assets/icons/arrow_down.png';
+import arrowUp from '../assets/icons/arrow_up.png';
+import { useState } from 'react'; //importer le Hook pour gérer l'état du composant//
+
+
+function Collapse ({title, content}) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleCollapse  =() => {setIsOpen(!isOpen);}
+
+    return (
+        <div className={`collapse ${isOpen ? 'open' : ''}`}>
+            <div className= "header-collapse" onClick={toggleCollapse}>
+                <h3>{title}</h3>
+                <img className='collapse-arrow'  src={isOpen ? arrowUp : arrowDown} alt="Flèche de direction" />
+            </div>
+            
+                <div className='collapse-content'>
+                    {content}
+                </div>
+            
+        </div>
+    )
+
+}
+
+export default Collapse;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import Layout from "../components/Layout";
 import Banner from "../components/Banner";
 import aboutBanner from '../assets/images/Image source 2.png'
+import Collapse from "../components/Collapse";
 
+    const collapseData = [
+    { title: "Fiabilité", content:  "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes. "},
+    { title: "Respect", content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."},
+    { title: "Service", content: ""},
+    { title: "Sécurité", content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."}
+];
 
 const About = () => {
     return (
         <Layout>
             <main>
                 <Banner image={aboutBanner}></Banner>
+                <section>
+                    {collapseData.map((item, index)=> (
+                        <Collapse key={index} title={item.title} content={item.content}></Collapse>
+                    ))}
+                    
+                </section>
             </main>
         </Layout>
     )

872735d64dea3099f4fe53c7e628879980d6698f
ImeneB7 <imene.bel74@gmail.com>
2024-06-22 09:48:20 +0200

    création du chemin des cards vers la page logement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
 import '../sass/_card.scss';
+import { Link } from 'react-router-dom';
 
 
 
 function Card({id, title, cover}) {
     return (
+        <Link to= {`/housing/${id}`} className='card_link' >
         <div className="card" id={id}>
             <img src={cover} alt={title} />
             <div>
                 <h2>{title}</h2>
             </div>
         </div>
+        </Link>
     )
 }
 

209cfc2f2ce3c1eb677e8bdab7d343d0d1083ece
ImeneB7 <imene.bel74@gmail.com>
2024-06-22 09:45:35 +0200

    ajout du chemin housing sur app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,7 @@ const App =() => {
                 <Routes>
                     <Route exact path="/" element={<Home/>}/>
                     <Route path="/about" element={<About/>}/>
-                    <Route path="/housing" element={<Housing/>}/>
+                    <Route path="/housing/:id" element={<Housing/>}/>
                     <Route path="*" element={<NotFound404/>}/>
                 </Routes>
         </Router>

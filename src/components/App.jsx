12e8553afbf23603dba0e63d2a26337b8b1de772
ImeneB7 <imene.bel74@gmail.com>
2024-06-25 15:58:49 +0200

    ajout du header et footer dans l'app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,22 @@ import Home from '../pages/Home';
 import About from '../pages/About';
 import NotFound404 from '../pages/NotFound404';
 import Housing from '../pages/Housing';
+import Header from "./Header";
+import Footer from "./Footer";
 
 
 
 const App =() => {
     return ( 
         <Router>
+            <Header/>
                 <Routes>
                     <Route exact path="/" element={<Home/>}/>
                     <Route path="/about" element={<About/>}/>
                     <Route path="/housing/:id" element={<Housing/>}/>
                     <Route path="*" element={<NotFound404/>}/>
                 </Routes>
+            <Footer/>
         </Router>
     )
 }
